d2cf0cb765e42591ed3e6eeccff6ac54553f7b1a
Flynn3094 <linhnguyen3094@gmail.com>
2016-08-15 18:25:40 +0700

    90%

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app=express();
 var mysql=require('mysql');
 var bodyParser=require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.set('view engine','ejs');
@@ -74,8 +75,29 @@ app.get('/submit',function(req,res){
 });
 
 app.post('/register',function(req,res){
-	console.log(req);
-	res.send(req);
+	var connection=mysql.createConnection({
+		host:'localhost',
+		user:'root',
+		password:'admin',
+		database:'app'
+	});
+	connection.connect(function(err){
+		if (err){
+			console.log(err);
+		}
+	});
+	var user={
+		name:req.body.name,
+		email:req.body.email,
+		job:req.body.job,
+		major:req.body.major
+	};
+	connection.query('INSERT INTO user SET ?',user,function(err,result){
+		if (err){
+			console.log(err);
+			res.status(500).json({error:err});
+		}
+	});
 });
 
 app.use(express.static(__dirname+'/static'));
